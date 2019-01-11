5f678d76ecc87b2d4a20e33cf478e6a50300a3d2
René Schultz Madsen <rm@microting.dk>
2019-01-11 14:13:30 +0100

    Adding the new testinginstallpn.sh

---
+++ testinginstallpn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i path: "rentable-items-pn",' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/rentable-items-pn/rentable-items-pn.module#RentableItemsPnModule"' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i
+
