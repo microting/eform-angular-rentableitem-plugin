5f56978fb3540cec577ec29729dfeaeea09dd0ec
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2020-01-06 09:57:40 +0100

    removing unnecessary check for activation of plugin.

--- eform-client/e2e/Tests/rentableitem-general/rentableitem/rentableItem-rentableItem-add.spec.ts
+++ eform-client/e2e/Tests/rentableitem-general/rentableitem/rentableItem-rentableItem-add.spec.ts
@@ -11,29 +11,6 @@ describe('Rentable Item Plugin - Rentable Item', function () {
     loginPage.open('/auth');
     loginPage.login();
   });
-  // it('should create eForm', function () {
-  //   const  neweFormLabel = 'Number 1';
-  //   rentableItemsPage.createNewEform(neweFormLabel);
-  // });
-  it('should check if activated', function () {
-    myEformsPage.Navbar.advancedDropdown();
-    myEformsPage.Navbar.clickonSubMenuItem('Plugins');
-    browser.pause(8000);
-    const plugin = pluginsPage.getFirstPluginRowObj();
-    expect(plugin.id).equal(1);
-    expect(plugin.name).equal('Microting Rentable Items plugin');
-    expect(plugin.version).equal('1.0.0.0');
-    expect(plugin.status).equal('Aktiveret');
-  });
-  it('should check if menu point is there', function () {
-    expect(rentableItemsPage.rentableItemDropdownName.getText()).equal('Lejelige ting');
-    rentableItemsPage.rentableItemDropdown();
-    browser.pause(4000);
-    expect(rentableItemsPage.rentableItemDropdownItemName('Lejelige ting').getText()).equal('Lejelige ting');
-    browser.pause(4000);
-    rentableItemsPage.rentableItemDropdown();
-    browser.refresh();
-  });
   it('should get btn text', function () {
   rentableItemsPage.goToRentableItemsPage();
   rentableItemsPage.getBtnTxt('New rentable item');
