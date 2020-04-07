e8af8681cb9b350981897b1507a3ca4d213d41ef
René Schultz Madsen <rm@microting.dk>
2020-04-07 08:39:08 +0200

    Fixing the paths rm'ed and copied.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -11,13 +11,13 @@ rm -fR Documents/workspace/microting/eform-angular-rentableitem-plugin/eFormAPI/
 cp -a Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/RentableItems.Pn Documents/workspace/microting/eform-angular-rentableitem-plugin/eFormAPI/Plugins/RentableItems.Pn
 
 # Test files rm
-rm -fR Documents/workspace/microting/eform-angular-rentable-items-plugin/eform-client/e2e/Tests/rentable-items-settings/
-rm -fR Documents/workspace/microting/eform-angular-rentable-items-plugin/eform-client/e2e/Tests/rentable-items-general/
+rm -fR Documents/workspace/microting/eform-angular-rentable-items-plugin/eform-client/e2e/Tests/rentableitem-settings/
+rm -fR Documents/workspace/microting/eform-angular-rentable-items-plugin/eform-client/e2e/Tests/rentableitem-general/
 rm -fR Documents/workspace/microting/eform-angular-rentable-items-plugin/eform-client/e2e/Page\ objects/Monitoring/
 rm -fR Documents/workspace/microting/eform-angular-rentable-items-plugin/eform-client/wdio-headless-plugin-step2.conf.js
 
 # Test files cp
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/rentable-items-settings Documents/workspace/microting/eform-angular-rentableitem-plugin/eform-client/e2e/Tests/rentableitem-settings
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/rentable-items-general Documents/workspace/microting/eform-angular-rentableitem-plugin/eform-client/e2e/Tests/rentableitem-general
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/rentableitem-settings Documents/workspace/microting/eform-angular-rentableitem-plugin/eform-client/e2e/Tests/rentableitem-settings
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/rentableitem-general Documents/workspace/microting/eform-angular-rentableitem-plugin/eform-client/e2e/Tests/rentableitem-general
 cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/rentableitem-general Documents/workspace/microting/eform-angular-rentableitem-plugin/eform-client/e2e/Page\ objects/rentableitem-general
 cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-rentableitem-plugin/eform-client/wdio-headless-plugin-step2.conf.js
