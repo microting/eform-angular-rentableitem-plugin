fee5002c224d3af3f812aad799e8206cb4cc3145
René Schultz Madsen <rm@microting.dk>
2026-03-25 08:57:58 +0100

    Fix FileNotFoundError: add existence check before copying test files

--- devinstall.py
+++ devinstall.py
@@ -66,7 +66,8 @@ for src_rel_path, dst_rel_path in test_files_to_copy:
     dst_path = os.path.join(dst_base, dst_rel_path)
 
     # Copy the source directory to the destination
-    if os.path.isdir(src_path):
-        shutil.copytree(src_path, dst_path)
-    else:
-        shutil.copy2(src_path, dst_path)
+    if os.path.exists(src_path):
+        if os.path.isdir(src_path):
+            shutil.copytree(src_path, dst_path)
+        else:
+            shutil.copy2(src_path, dst_path)
